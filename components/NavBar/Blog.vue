0a23ab6fa3b7c822b744233ed131fce4f9eeec92
Mehmetali345 <mehmetali345@mehmetali345.xyz>
2021-06-22 17:01:29 +0300

    Website files uploaded.

---
+++ components/NavBar/Blog.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex justify-between p-8 text-center text-sm">
+  <a class="px-8" href="/"><nuxt-img class="w-16 h-16 rounded-full" src="wardbot.png"></nuxt-img></a>
+    <div class="p-1">
+      <a class="text-blue-400 p-1" href="/">Ana Sayfa</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ components/NavBar/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex items-center p-8 text-center text-sm">
+  <a class="px-8" href="/"><nuxt-img class="w-16 h-16 rounded-full" src="wardbot.png"></nuxt-img></a>
+    <div class="p-1">
+      <a
+        class="text-blue-400 p-1"
+        target="_blank"
+        href="https://discord.com/oauth2/authorize?client_id=846045254750175312&scope=bot&permissions=8"
+        >Botu Ekle</a
+      >      
+      <a href="https://discord.gg/jWdYXR99Va" class="text-blue-400 p-1">Destek Sunucusu</a>
+      <a class="text-blue-400 p-1" href="https://docs.wardbot.net">Dokümentasyon</a>
+      <a class="text-blue-400 p-1" href="blog/">Blog</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
